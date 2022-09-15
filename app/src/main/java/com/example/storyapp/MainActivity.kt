089fa4757407c2b7fab3d38f086e28172df34b06
Firman Fachrial <firman.fachrial@gmail.com>
2022-09-15 08:21:11 +0700

    - Change buildSDK to 33 - Add class CustomButton with constructor and onDraw function

--- app/src/main/java/com/example/storyapp/MainActivity.kt
+++ app/src/main/java/com/example/storyapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.storyapp
 
+import android.content.Context
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+
+class CustomButton : AppCompatButton {
+    constructor(context: Context) : super(context) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
     }
 }
